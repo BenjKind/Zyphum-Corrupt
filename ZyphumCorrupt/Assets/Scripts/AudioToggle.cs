51e7b169923d58523b9fafdfbd2753a5c139d067
Benjamin Kindle <benjcskindle@gmail.com>
2020-12-26 16:30:53 -0800

    Added more UI buttons, integrated audio toggle button and prefabs folder

---
+++ ZyphumCorrupt/Assets/Scripts/AudioToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(Toggle))]
+public class AudioToggle : MonoBehaviour
+{
+    private Toggle AudioToggler;
+
+    public void ToggleAudio()
+    {
+        AudioToggler = GetComponent<Toggle>();
+        if (AudioListener.volume == 0)
+        { AudioToggler.isOn = false; }
+    }
+
+    public void ToggleAudioOnValueChange(bool audioIn)
+    {
+        if (audioIn)
+        { AudioListener.volume = 0; }
+        else
+        { AudioListener.volume = 1; }
+    }
+}
