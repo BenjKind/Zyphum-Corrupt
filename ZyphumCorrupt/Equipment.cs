50645ba0ed3fdd26e718d1dbc7a97b96da060e21
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2020-12-31 17:46:16 -0600

    Created Equioment Object with simple memers and getters setters

---
+++ ZyphumCorrupt/Equipment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Equipment
+{
+    #region Members
+
+    //This will be used to add to character stats temporarily
+    private int boost;
+    //This will be sourced from a dictionary in global namespace
+    private string boostType;
+
+    #endregion
+
+    #region Getters Setters
+
+    public int GetBoost()
+    {
+        return boost;
+    }
+    public void SetBoost(int interger)
+    {
+        boost = interger;
+    }
+    public string GetBoostType()
+    {
+        return boostType;
+    }
+    public void SetBoostType(string type)
+    {
+        boostType = type;
+    }
+
+    #endregion
+
+    public Equipment()
+	{
+        boost = 0;
+        boostType = "Null";
+	}
+}
