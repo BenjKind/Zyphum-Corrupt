ae1f7f487faa7c3533cc0b87f1c0a0857696fbf4
Benjamin Kindle <benjcskindle@gmail.com>
2021-01-11 22:28:28 -0800

    Corrected function to load UserName, to dynamically change it from user input will be added down the road.

--- ZyphumCorrupt/Assets/Scripts/displayUsername.cs
+++ ZyphumCorrupt/Assets/Scripts/displayUsername.cs
@@ -2,15 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class displayUsername : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void Start()
+    public void Start()
     {
-        GameObject.Find("yay").GetComponent<NewBehaviourScript>().Arachnae.Name;
-        //GameObject userNameImage = GameObject.Find("displayUserName");
-        //User test = userNameImage.GetComponent<User>();
-        //gameObject.GetComponent<UnityEngine.UI.Text>().text = userNameImage.Name;
+        User nameStuff = new User();
+        Serializer serialize = new Serializer("Dump.xml");
+        serialize.DeserializeUser(out nameStuff);
+        GameObject.Find("displayUserName").GetComponent<TextMeshProUGUI>().SetText(nameStuff.Name);
     }
 }
