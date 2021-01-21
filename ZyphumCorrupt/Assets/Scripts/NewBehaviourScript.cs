52d4969edcb550145c40d8e76f3ff9e33ec0a363
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2021-01-21 16:08:41 -0600

    created character xml's for Noir, Kushimani, Luthren, Layla, and Blane

--- ZyphumCorrupt/Assets/Scripts/NewBehaviourScript.cs
+++ ZyphumCorrupt/Assets/Scripts/NewBehaviourScript.cs
@@ -15,6 +15,8 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void dothisthing()
     {
+        Serializer sz = new Serializer();
+
         Character Noir = new Character();
         Noir.Name = "Noir";
         Noir.Class = "Red Mage";
@@ -25,7 +27,59 @@ public class NewBehaviourScript : MonoBehaviour
         Noir.Defense = 13;
         Noir.Magic = 70;
         Noir.MagicDefense = 68;
-        Serializer sz = new Serializer();
-        sz.SerializeCharacter(Noir);
+        Character Kushimani = new Character();
+        Kushimani.Name = "Kushimani";
+        Kushimani.Class = "Black Mage";
+        Kushimani.ElementalAffinity = "Void";
+        Kushimani.MaxHealth = 22;
+        Kushimani.Speed = 57;
+        Kushimani.Attack = 26;
+        Kushimani.Defense = 13;
+        Kushimani.Magic = 88;
+        Kushimani.MagicDefense = 76;
+        Character Luthren = new Character();
+        Luthren.Name = "Luthren Alanaust";
+        Luthren.Class = "Rune Blade";
+        Luthren.ElementalAffinity = "Fire";
+        Luthren.MaxHealth = 76;
+        Luthren.Speed = 62;
+        Luthren.Attack = 66;
+        Luthren.Defense = 48;
+        Luthren.Magic = 76;
+        Luthren.MagicDefense = 74;
+        Character Layla = new Character();
+        Layla.Name = "Layla the Wolf";
+        Layla.Class = "White Mage";
+        Layla.ElementalAffinity = "Fire";
+        Layla.MaxHealth = 16;
+        Layla.Speed = 88;
+        Layla.Attack = 12;
+        Layla.Defense = 14;
+        Layla.Magic = 94;
+        Layla.MagicDefense = 95;
+        Character Blane = new Character();
+        Blane.Name = "Blane Garashina";
+        Blane.Class = "Brawler";
+        Blane.ElementalAffinity = "Ice";
+        Blane.MaxHealth = 76;
+        Blane.Speed = 22;
+        Blane.Attack = 58;
+        Blane.Defense = 87;
+        Blane.Magic = 14;
+        Blane.MagicDefense = 43;
+
+        List<Character> characters = new List<Character>();
+        characters.Add(Noir);
+        characters.Add(Kushimani);
+        characters.Add(Luthren);
+        characters.Add(Layla);
+        characters.Add(Blane);
+
+        foreach(Character person in characters)
+        {
+            sz.SerializeCharacter(person);
+        }
+
+
     }
 }
