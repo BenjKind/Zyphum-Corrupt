a9b9498c546015f7d697c52541722dba705f138f
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2021-01-21 15:54:18 -0600

    Serilizing Noir

--- ZyphumCorrupt/Assets/Scripts/NewBehaviourScript.cs
+++ ZyphumCorrupt/Assets/Scripts/NewBehaviourScript.cs
@@ -15,6 +15,17 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void dothisthing()
     {
-        
+        Character Noir = new Character();
+        Noir.Name = "Noir";
+        Noir.Class = "Red Mage";
+        Noir.ElementalAffinity = "Ice";
+        Noir.MaxHealth = 20;
+        Noir.Speed = 44;
+        Noir.Attack = 53;
+        Noir.Defense = 13;
+        Noir.Magic = 70;
+        Noir.MagicDefense = 68;
+        Serializer sz = new Serializer();
+        sz.SerializeCharacter(Noir);
     }
 }
