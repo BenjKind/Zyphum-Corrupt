c7b56725375d6b1a7705fb5084bcbf0e6f47a8cb
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2021-01-31 18:00:01 -0600

    Gave user currency also shows in shop prepped to create a modular shop menu

--- ZyphumCorrupt/Assets/Scripts/ZyphumScript.cs
+++ ZyphumCorrupt/Assets/Scripts/ZyphumScript.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class ZyphumScript : MonoBehaviour
 {
     public User currentUser = new User();
+
+    List<Equipment> equip = new List<Equipment>();
+    List<Item> items = new List<Item>();
+
+
+
+
     private void Awake()
     {
+        string[] equipFiles;
+        string[] itemFiles;
+        equipFiles = Directory.GetFiles("Equipment", "*.xml");
+        itemFiles = Directory.GetFiles("Items", "*.xml");
+        Serializer sz = new Serializer();
+        foreach(string str in equipFiles)
+        {
+            Equipment equipment = new Equipment();
+            sz.DeserializeEquioment(str, out equipment);
+            equip.Add(equipment);
+        }
+        foreach (string str in itemFiles)
+        {
+            Item item = new Item();
+            sz.DeserializeItem(str, out item);
+            items.Add(item);
+        }
+
+
         SceneManager.LoadScene(1, LoadSceneMode.Additive);
     }
 
