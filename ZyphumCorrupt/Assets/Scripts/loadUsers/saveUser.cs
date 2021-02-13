1e588de079e2a64fe8c1db1625b93777ba17cbba
Benjamin Kindle <benjcskindle@gmail.com>
2021-02-13 14:16:57 -0800

    Cleaned up unnecessary scripts

--- ZyphumCorrupt/Assets/Scripts/loadUsers/saveUser.cs
+++ ZyphumCorrupt/Assets/Scripts/loadUsers/saveUser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class newUser : MonoBehaviour
+public class saveUser : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
