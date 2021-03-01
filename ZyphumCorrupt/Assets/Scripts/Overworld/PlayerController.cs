80a6b004fdfacb72577a5b5c70ac4864f61ee1ab
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2021-02-28 19:32:38 -0600

    Player can move now added a triger listener on the player controller script

--- ZyphumCorrupt/Assets/Scripts/Overworld/PlayerController.cs
+++ ZyphumCorrupt/Assets/Scripts/Overworld/PlayerController.cs
@@ -8,15 +8,26 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb;
+    private Vector2 moveVelo;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Verticle"));
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelo = moveInput.normalized * speed;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelo * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
     }
 }
